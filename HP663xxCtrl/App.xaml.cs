cb039db996533b85641f4573550eb3c418188e94
Nathan Conrad <conradn@purdue.edu>
2017-09-24 22:25:43 -0400

    Update to Visual Studio 2017, and target .Net 4.5.2.

--- HP663xxCtrl/App.xaml.cs
+++ HP663xxCtrl/App.xaml.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Threading;
 using System.Windows;
 using HP663xxCtrl;
 namespace HP663xxCtrl
@@ -12,6 +14,11 @@ namespace HP663xxCtrl
     /// Interaction logic for App.xaml
     /// </summary>
     public partial class App : Application
-    {
+    { 
+        public App() {
+            /*CultureInfo culture = CultureInfo.CreateSpecificCulture("fr-FR");
+            System.Threading.Thread.CurrentThread.CurrentUICulture = culture;
+            System.Threading.Thread.CurrentThread.CurrentCulture = culture;*/
+        }
     }
 }
